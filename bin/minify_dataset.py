3ea961c2cfd8f259bdd8173746980bfd0a33fc41
Bradford Condon <bradford.condon@gmail.com>
2018-07-24 13:01:03 -0400

    put together shell script

---
+++ bin/minify_dataset.py
@@ -0,0 +1,73 @@
+from Bio import SeqIO
+import sys
+from os import path
+import csv
+
+
+def main(argv):
+    usage = 'split_fasta.py [n] [mrna FASTA] [polypeptide FASTA] [polypeptide regexp] [GFF]'
+    out_count = 1
+    if len(argv) < 4:
+        print usage
+        sys.exit()
+    n = argv[0]
+    mrna = argv[1]
+    polyp = argv[2]
+    regexp = argv[3]
+    gff = argv[4]
+    selected_mrna = []
+
+    mrna_path = path.relpath(mrna)
+    polyp_path = path.relpath(polyp)
+    gff_path = path.relpath(gff)
+
+    ofh = open("mrna_mini.fasta", "w")
+    count = 0
+
+    print ("opening " + mrna_path)
+
+    fasta_sequences = SeqIO.parse(open(mrna_path), 'fasta')
+    for fasta in fasta_sequences:
+        if count < n:
+            name, sequence = fasta.id, str(fasta.seq)
+            ofh.write('>' + name + '\n' + sequence + '\n')
+            count += 1
+            selected_mrna.append(name)
+    ofh.close()
+
+    simple_gff_trimmer(gff_path, selected_mrna)
+
+
+def simple_gff_trimmer(gff_path, selected_mrna):
+    with open(gff_path, 'rb') as tsvin, open('filtered.gff', 'wb') as gffout:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        csvout = csv.writer(gffout, delimiter='\t')
+        csvout.writerow(['##gff-version 3'])
+        write = False
+
+        for row in tsvin:
+            if len(row) > 1:
+                contig = row[0]
+                type = row[2]
+                info = row[-1]
+                if type == 'mRNA':
+                    write = False
+                    info_split = info.split(';')
+                    id = info_split[0]
+                    id = id.replace('ID=', '')
+                    if id in selected_mrna:
+                        write = True
+                        csvout.writerow(row)
+                else:
+                    if write:
+                        csvout.writerow(row)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+    """
+    Take a mrna and GFF file.  
+    Pick N mRNA at random and trim everything down to that.
+
+    """
--- create_new_data/run.sh
+++ create_new_data/run.sh
@@ -1,32 +1,56 @@
+#!/bin/bash
+
+MRNA_FASTA =  $1
+PROTEIN_FASTA =  $2
+PROTEIN_REGEXP =  $3
+GFF = $4
+COUNT = $5
+DB_DIR = $6
+
+mkdir -p out/sequences
+
+#Trim input files
+
+python ../bin/minify_dataset.py \
+ $COUNT $MRNA_FASTA $PROTEIN_FASTA $PROTEIN_REGEXP $GFF
 
 #blast nucleotide against TREMBL
 blastx \
--query $input_nuc \
--db /db/trembl.fasta \
+-query out/sequences/mrna_mini.fasta \
+-db "$DB_DIR/trembl.fasta"\
 -out /out/TREMBL_$input_nuc.xml  \
 -evalue 1e-5 \
 -outfmt 5
 #blast nucleotide against SPROT
 
 blastx \
--query $input_nuc \
--db /db/uniprot_sprot.fasta \
+-query out/sequences/mrna_mini.fasta \
+-db "$DB_DIR/uniprot_sprot.fasta" \
 -out /out/SPROT_$input_nuc.xml  \
 -evalue 1e-5 \
 -outfmt 5
 
+mkdir -p out/ips
+
 #run IPS
 interproscan.sh \
--i $input_prot \
+-i out/sequences/polypeptide_mini.fasta \
 -f XML, tsv \
 -d /out/ips/ \
 --disable-precalc \
 --iprlookup \
 --goterms \
 --pathways \
-# --tempdir /lustre/projects/staton/projects/hwg_tmp_anlaysis_files/Jregia/ips/TMP \
-# >& /lustre/projects/staton/projects/hwg_tmp_anlaysis_files/Jregia/ips/TMP/$SGE_TASK_ID.out
+# --tempdir /TMP
 
 #generate Biomaterials
 
+mkdir -p out/biomaterials
+
+python ../bin/generate_biomaterials.py
+
+
 #Create false expression data
+mkdir -p out/expression
+
+python ../bin/generate_expression.py
