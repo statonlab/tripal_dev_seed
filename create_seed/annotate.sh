50c1d9c8a6bb5f7fff945823b8d2636a149ba635
Bradford Condon <bradford.condon@gmail.com>
2018-07-25 11:11:54 -0400

    split annotate and minify

---
+++ create_seed/annotate.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+MINI_MRNA_FASTA=$1
+MINI_PROTEIN_FASTA=$2
+DB_DIR=$3
+PREFIX=$4
+
+if [ -d "out/ips" ]; then
+rm -rf out/ips
+fi
+if [ -d "out/blast" ]; then
+rm -rf out/blast
+fi
+
+#blast nucleotide against TREMBL
+blastx \
+-query $MINI_MRNA_FASTA \
+-db "$DB_DIR/trembl.fasta"\
+-out "/out/blast/$PREFIX.TREMBL.xml"  \
+-evalue 1e-5 \
+-outfmt 5
+#blast nucleotide against SPROT
+
+blastx \
+-query $MINI_MRNA_FASTA \
+-db "$DB_DIR/uniprot_sprot.fasta" \
+-out "/out/$PREFIX.SPROT.xml"  \
+-evalue 1e-5 \
+-outfmt 5
+
+mkdir -p out/ips
+
+#run IPS
+interproscan.sh \
+-i $MINI_PROTEIN_FASTA \
+-f XML, tsv \
+-d /out/ips/ \
+--disable-precalc \
+--iprlookup \
+--goterms \
+--pathways \
+# --tempdir /TMP
+
+#generate Biomaterials
+
+mkdir -p out/biomaterials
+
+python ../bin/generate_biomaterials.py 20
+
+
+#Create false expression data
+mkdir -p out/expression
+
+python ../bin/generate_expression.py out/biomaterials/biomaterials.xml $MINI_MRNA_FASTA
