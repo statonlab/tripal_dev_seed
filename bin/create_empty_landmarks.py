7b2121dd24ab7a89cca4da08d266070c3b90d597
Bradford Condon <bradford.condon@gmail.com>
2018-07-26 15:55:35 -0400

    create empty landmark script

---
+++ bin/create_empty_landmarks.py
@@ -0,0 +1,28 @@
+import sys, re, csv
+from os import path
+
+def main(argv):
+    usage = 'create_empty_landmarks.py [GFF file]'
+    if len(argv) < 1:
+        print usage
+        sys.exit()
+
+    gff = argv[0]
+    contigs = {}
+    with open(gff_path, 'rb') as tsvin:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        if len(row) > 1:
+            contig = row[0]
+            contigs[contig] = contig
+
+    with open('out/sequences/empty_landmarks.fasta', 'wb') as landmark_out:
+        for contig in contigs:
+            print ">contig\n"
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+    """
+
+    """
--- bin/minify_dataset.py
+++ bin/minify_dataset.py
@@ -2,6 +2,7 @@ from Bio import SeqIO
 import sys, re, csv
 from os import path
 import trim_polypeptide
+import create_empty_landmarks
 
 
 def main(argv):
@@ -41,6 +42,9 @@ def main(argv):
     print ("trimming GFF file")
     simple_gff_trimmer(gff_path, selected_mrna, gff_mrna_key)
 
+    print ("creating landmark FASTA file")
+    create_empty_landmarks.main(["out/gff/filtered.gff"])
+
     print ("trimming polypeptides")
 
     trim_polypeptide.main([mrna_path, polyp, regexp])
@@ -52,17 +56,13 @@ def simple_gff_trimmer(gff_path, selected_mrna, gff_mrna_key):
         csvout.writerow(['##gff-version 3'])
         write = False
 
-<<<<<<< HEAD
         stored_genes = {}
 
-=======
->>>>>>> a1d67450ede755fd16cd14e51b1914f4458195cd
         for row in tsvin:
             if len(row) > 1:
                 contig = row[0]
                 type = row[2]
                 info = row[-1]
-<<<<<<< HEAD
                 if type == 'gene':  #Need to track parents
                     id = find_key(info, 'ID=')
                     stored_genes[id] = row
@@ -80,26 +80,11 @@ def simple_gff_trimmer(gff_path, selected_mrna, gff_mrna_key):
                             print "Error!  Could not find parent " + parent + ".\n  Please check your GFF file."
                             exit()
                         csvout.writerow(row)
-=======
-                if type == 'mRNA':
-                    write = False
-                    info_split = info.split(';')
-                    for item in info_split:
-                        id = None
-                        match = re.match(gff_mrna_key, item)
-                        if hasattr(match, 'group'):
-                            id = item.replace(gff_mrna_key, '')
-                    if id in selected_mrna:
-                        write = True
-                        csvout.writerow(row)
-                        #we also need to go back and write out the gene.
->>>>>>> a1d67450ede755fd16cd14e51b1914f4458195cd
                 else:
                     if write:
                         csvout.writerow(row)
 
 
-<<<<<<< HEAD
 def find_key(info, key):
     info_split = info.split(';')
     result = None
@@ -110,9 +95,6 @@ def find_key(info, key):
             result = id
     return result
 
-
-=======
->>>>>>> a1d67450ede755fd16cd14e51b1914f4458195cd
 if __name__ == "__main__":
     main(sys.argv[1:])
 
