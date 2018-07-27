a3a8e968b41332c59291d172e88a6d95726f70fc
Bradford Condon <bradford.condon@gmail.com>
2018-07-26 21:09:04 -0400

    change biomaterial IDs to Ids

--- bin/generate_biomaterials.py
+++ bin/generate_biomaterials.py
@@ -17,7 +17,7 @@ def create_biomaterials(num_biomats):
         accession = fake.word()
         biosample = ET.SubElement(root, "BioSample", submission_date=fake.date(), id=fake.word(), accession=accession)
 
-        ET.SubElement(biosample, "IDs", db="BioSample", is_primary="1").text = accession
+        ET.SubElement(biosample, "Ids", db="BioSample", is_primary="1").text = accession
         ET.SubElement(biosample, "Description").text = fake.sentence()
         owner = ET.SubElement(biosample, "Owner")
         ET.SubElement(owner, "Name", ).text = fake.name()
